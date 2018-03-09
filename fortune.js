f9d1c9ce5591f39f1616e993a34ddbd7742e8c54
MchlPersin <mlp218@exeter.ac.uk>
2018-03-09 13:58:37 +0000

    Broke code to create conflict

--- fortune.js
+++ fortune.js
@@ -4,7 +4,7 @@ var express = require( "express" )
    var request = require( "request" )
    var app = express()
    var www_yerkee_com = "68.71.131.28"
-   app.get( "/fortune/", function ( reqt, resp ) {
+   app.get( "/fortune/", functr`raararaagfagion ( reqt, resp ) {
      request( "http://" + www_yerkee_com + "/api/fortune",
        function( err, hdr, body ) {
          resp.json( bdy )

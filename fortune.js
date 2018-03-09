142b638fa685f4c4130be0ba385cc5f2832c3c33
MchlPersin <mlp218@exeter.ac.uk>
2018-03-09 14:06:32 +0000

    broke code

--- fortune.js
+++ fortune.js
@@ -7,7 +7,7 @@ var express = require( "express" )
    app.get( "/fortune/", functr`raararaagfagion ( reqt, resp ) {
      request( "http://" + www_yerkee_com + "/api/fortune",
        function( err, hdr, body ) {
-         resp.json( bdy )
+         resp.json( hfugaiyigfsiyo gbdy )
 }) })
    app.listen( 3000, function () {
      console.log( "Listening on port 3000..." )
